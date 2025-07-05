db6230fcd04a81bfe629745a693e6f4ac5f2efd8
im1maheshwaran <maheshwaranpalanisamy1@gmail.com>
2025-07-05 14:49:37 +0530

    Initial commit for clean deployment

---
+++ todo_project/login_form.js
@@ -0,0 +1,120 @@
+let new_user_text = document.querySelector('.new-user-text');
+
+let under_text = document.querySelector('.under-text');
+
+let sign_up_button = document.querySelector('.signup');
+
+let main_text = document.querySelector('.main-text');
+
+let form_tag = document.querySelector('.form');
+
+let button_tag = document.querySelector('.button');
+
+let isSignUpMode = false;
+
+
+function changeFunction() {
+    if(isSignUpMode){
+        new_user_text.innerHTML = 'Already Have An Account ?';
+
+        under_text.innerHTML = '';
+
+        sign_up_button.innerHTML = '< Back To LogIn';
+
+        main_text.innerHTML = 'Sign Up With';
+
+        form_tag.innerHTML = '<input type="text" placeholder="Your Name" class="userName"><input type="email" placeholder="Email" class="userEmail"><input type="password" placeholder="Create Password" class="userPassword"><input type="password" placeholder="Confirm Password" class="userRePassword">';
+        
+        button_tag.innerHTML = '<button id="signed_up">Sign Up</button>';
+
+
+        
+        document.getElementById('signed_up').addEventListener('click', function(){
+
+            let UserName = document.querySelector('.userName').value;
+
+            let UserEmail = document.querySelector('.userEmail').value;
+
+            let UserPassword = document.querySelector('.userPassword').value;
+            
+            let UserRePassword = document.querySelector('.userRePassword').value;
+            
+            if(!UserName ||!UserEmail || !UserPassword || !UserRePassword){
+                alert('Please fill in all fields');
+                return;
+            }
+
+            if(UserPassword !== UserRePassword){
+                alert("Passwords doesn't match");
+                return;
+            }
+
+            let users = JSON.parse(localStorage.getItem('users')) || [];
+
+            for(let i=0;i<users.length;i++){
+                if(users[i].email === UserEmail){
+                    alert('This E-mail already exists');
+                    return;
+                }
+            }
+            
+            users.push({name : UserName , email : UserEmail, password : UserPassword});
+            
+            localStorage.setItem('users', JSON.stringify(users));
+
+            sessionStorage.setItem("activeUserName",UserName)
+
+            window.location.href = 'http://127.0.0.1:5500/todo_project/todo.html';
+        });
+    }
+
+    else{
+        new_user_text.innerHTML = 'New User ?';
+
+        under_text.innerHTML = "Sign up here and make your TODO's list";
+        
+        sign_up_button.innerHTML = 'Sign Up >';
+        
+        main_text.innerHTML = 'Log In With';
+        
+        form_tag.innerHTML = '<input type="email" placeholder="Email" class="loggedin-email"><input type="password" placeholder="Password" class="loggedin-password">';
+        
+        button_tag.innerHTML = '<button id="loggedin_up">Log In</button>';
+        
+        document.getElementById('loggedin_up').addEventListener('click', function(){
+
+            let Loggedin_Email = document.querySelector('.loggedin-email').value.trim();
+            
+            let Loggedin_Password = document.querySelector('.loggedin-password').value.trim();
+
+            let users = JSON.parse(localStorage.getItem('users')) || [];
+
+            let found = false;
+            
+            for(let i=0;i<users.length;i++){
+                if(users[i].email === Loggedin_Email && users[i].password === Loggedin_Password){
+                    found = true;
+                    sessionStorage.setItem("activeUser", Loggedin_Email);
+                    sessionStorage.setItem("activeUserName",users[i].name)
+                    break;
+                }
+            }
+            
+            if(found){
+                window.location.href = 'http://127.0.0.1:5500/todo_project/todo.html';
+            }
+            else{
+                alert('Incorrect E-mail or Password');
+            }
+
+            console.log(JSON.parse(localStorage.getItem("users")));
+            
+        });
+    }
+}
+
+function toggleSignUp(){
+    isSignUpMode = !isSignUpMode;
+    changeFunction();
+}
+changeFunction();
---
+++ todo_project/todo.js
@@ -0,0 +1,146 @@
+let activeUser = sessionStorage.getItem('activeUser');
+
+    console.log(`Welcome ${activeUser}`);
+
+    let userName = sessionStorage.getItem("activeUserName");
+
+    document.querySelector('.userGreetings').innerHTML = `👋&nbsp;<span class="username-highlight">${userName}&nbsp;</span><span>!</span>`;
+
+    document.querySelector('.logout_button').addEventListener("click", function(){
+        sessionStorage.removeItem('activeUser');
+        sessionStorage.removeItem('activeuserName');
+        window.location.href = 'http://127.0.0.1:5500/index.html';
+    })
+
+    window.addEventListener('DOMContentLoaded',function(){
+        const savedTheme = this.localStorage.getItem('themePreference');
+
+        if(savedTheme === 'dark'){
+            document.body.classList.add('dark-theme');
+            document.querySelector('i').className = 'bx bx-sun';
+        }
+        else{
+            document.body.classList.remove('dark-theme');
+            document.querySelector('i').className('bx bx-moon');
+        }
+    });
+
+    let inputText = document.querySelector(".input");
+
+    let dateInput = document.querySelector(".date");
+
+    let timeInput = document.querySelector(".time");
+
+    let inputArray = JSON.parse(localStorage.getItem(activeUser + '_inputArray')) || [];
+
+    let dateArray = JSON.parse(localStorage.getItem(activeUser + '_dateArray')) || [];
+
+    let timeArray = JSON.parse(localStorage.getItem(activeUser + '_timeArray')) || [];
+
+    addtodo();
+
+    function addtodo() {
+
+        let displayDiv = document.querySelector(".division");
+
+        let todolist = "";
+
+        for (let i = 0; i < inputArray.length; i++) {
+            const dispaly = `
+            <div class="todo_card">
+            <div>${inputArray[i]}</div>
+            <div>${dateArray[i]}</div>
+            <div>${timeArray[i]}</div>
+            <button class="
+                edit" onclick="
+                inputArray[${i}] = prompt('Edit your task:', '${inputArray[i]}')
+                dateArray[${i}] = prompt('Edit you date:','${dateArray[i]}');
+                timeArray[${i}] = prompt('Edit you time:','${timeArray[i]}');
+                saveTodos();
+                addtodo();
+            ">EDIT</button>
+            <button class="
+                delete" onclick="
+                inputArray.splice(${i}, 1)
+                dateArray.splice(${i},1);
+                timeArray.splice(${i},1);
+                saveTodos();
+                addtodo();
+            ">DELETE</button>
+            </div>
+            `;
+            todolist += dispaly;
+        }
+        displayDiv.innerHTML = todolist;
+        
+    }
+    
+    function add() {
+
+        console.log("button clicked");
+        
+        if(!inputText.value || !dateInput.value || !timeInput.value){
+            alert('Please fill in all fields');
+            return;
+        }
+        inputArray.push(inputText.value);
+
+        dateArray.push(dateInput.value);
+
+        timeArray.push(timeInput.value);
+
+        saveTodos();
+
+        inputText.value = "";
+        dateInput.value = "";
+        timeInput.value = "";
+        addtodo();
+    }
+
+    function saveTodos() {
+
+        localStorage.setItem(activeUser + '_inputArray', JSON.stringify(inputArray));
+
+        localStorage.setItem(activeUser + '_dateArray', JSON.stringify(dateArray));
+
+        localStorage.setItem(activeUser + '_timeArray', JSON.stringify(timeArray));
+
+    }
+
+    function theme() {
+        document.body.classList.toggle('dark-theme');
+
+        const icon = document.querySelector('i');
+
+        let isDark = document.body.classList.contains('dark-theme');
+        
+        localStorage.setItem('themePreference', isDark? 'dark' : 'light');
+
+        if(isDark){
+            icon.className = 'bx bx-sun';
+        }
+        else{
+            icon.className = 'bx bx-moon';
+        }
+
+    }
+
+    function clearAll(){
+        if(confirm("Are you sure you want to delete all your TODOs?")) {
+            inputArray = [];
+            dateArray = [];
+            timeArray = [];
+            saveTodos();
+            addtodo();
+        }
+    }
+
+    document.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        if (inputText.value.trim() && dateInput.value && timeInput.value.trim()) {
+            add(); 
+        } else {
+            alert("Please fill in all fields before pressing Enter.");
+        }
+    }
+});
